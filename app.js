722568181cb010b69f626fba65ef4fb16c5b65d1
Stamatios Theodosiadis <stheodosi@csd.auth.gr>
2020-10-15 02:54:45 +0300

    new

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 const client = new Discord.Client();
 
-const token = 'NzY1NDExODEyNTA1NjgxOTYw.X4UbTA.jW0lciOeIjRtZzErvsB9AnMNvT8';
+const token = 'YourToken';
 const prefix = '.';
 
 client.on('ready', () => {
